91d9eaa8de20aacb6c120d390bf3d7d4ab150ed1
Ned Zimmerman <ned@bight.ca>
2015-11-03 14:21:19 -0500

    Removed hard-coded Google Analytics snippet.

--- header.php
+++ header.php
@@ -14,17 +14,6 @@
 <script src="<?php bloginfo('template_url') ?>/js/selectivizr.js"></script>
 <![endif]-->
 
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-55411861-1', 'auto');
-  ga('send', 'pageview');
-
-</script>
-
 </head>
 <body <?php body_class() ?>>
 
