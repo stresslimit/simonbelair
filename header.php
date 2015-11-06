0678301bce344d3b6b924030dd9d356e9952ca5b
Ned Zimmerman <ned@bight.ca>
2015-11-06 12:12:34 -0400

    Update header.php

--- header.php
+++ header.php
@@ -43,7 +43,7 @@
 		} ?>
 	<div class="fb-like" 
         	data-href="https://facebook.com/SimonBelairAc" 
-        	data-layout="standard" 
+        	data-layout="button_count" 
         	data-action="like" 
         	data-show-faces="false">
     	</div>
